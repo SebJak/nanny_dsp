2ee9d74c83d2c27cff85fc64563152b103ffa759
Sebastian J <sjak90@gmail.com>
2017-05-06 21:27:05 +0200

    Add client and detector button and simple view for each mode.

---
+++ Nanny/app/src/main/java/com/dsp/sjk/nanny/ClientActivity.java
@@ -0,0 +1,16 @@
+package com.dsp.sjk.nanny;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by seba on 06.05.17.
+ */
+public class ClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+    }
+}
---
+++ Nanny/app/src/main/java/com/dsp/sjk/nanny/DetectorActivity.java
@@ -0,0 +1,17 @@
+package com.dsp.sjk.nanny;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by seba on 06.05.17.
+ */
+public class DetectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detector);
+    }
+
+}
